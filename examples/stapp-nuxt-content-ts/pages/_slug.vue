260cbecd7556d954e45245406dc68cb5b1684adf
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-09-05 07:46:09 +0900

    add _slug

---
+++ examples/stapp-nuxt-content-ts/pages/_slug.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="container mx-auto my-8">
+    <h1 class="text-4xl font-bold mb-2">{{ page.title }}</h1>
+    <p class="mb-4 text-xl">{{ page.date }}</p>
+    <hr />
+    <nuxt-content :document="page" class="my-6" />
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Context } from '@nuxt/types'
+import { Article } from '@/types/article'
+
+export default Vue.extend({
+  async asyncData({ $content, params }: Context) {
+    const page = await $content('/', params.slug).fetch<Article>()
+    return { page }
+  },
+  data() {
+    return { page: {} as Article }
+  },
+})
+</script>
